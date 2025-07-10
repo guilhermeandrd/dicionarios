6d4c55740e80333050b24f9cb56465dc0456c2c0
guilhermeandrd <nickyandrade331@gmail.com>
2025-07-10 10:08:32 -0300

    comecei a fazer o arquivo principal

---
+++ freq.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    cout << "numero de parametros = " << argc << endl;
+
+    for(int i = 0; i < argc; ++i) {
+        cout << "argumento [" << i << "] = " << argv[i] << endl;
+    } 
+
+    //primeiro parametro
+
+    //segundo - tipo da estrutra a ser utilizada
+
+    //terceiro - string a ser lida
+
+    return 0;
+}
